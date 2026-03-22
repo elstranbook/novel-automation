7dd714aee92296736bdd8bb6785a727965e32ce1
Micheal Kiwanuka <elstranapps@gmail.com>
2026-03-22 19:43:53 +0300

    Fix: Series build error2

--- web/src/app/series/page.tsx
+++ web/src/app/series/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { createSupabaseBrowserClient } from "@/lib/supabaseClient";
 
